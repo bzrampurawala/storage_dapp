b3b4c7f1010a3077d2620b00e9a2807f4b580e59
Burhan <bzrampurawala@gmail.com>
2019-02-24 18:23:06 +0530

    added file holder to display files

--- view/src/js/index.js
+++ view/src/js/index.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import fileObject from './utils/storeFiles';
+import AddButton from './ui/addButton';
+import FileHolder from './ui/fileHolder';
 const app = document.getElementById('main');
 class Main extends React.Component{
-    handleOnClick(e){
-        e.preventDefault()
-        e.stopPropagation()
 
-        const files = e.currentTarget.files;
-        fileObject.upload(files[0]);
-    }
     render(){
         return(
-        <div>
-            <label className='addButton'>
-            Add File
-                <input accept='true' autoComplete='off' className = 'fileInput' type="file" tabIndex='-1' onChange = {this.handleOnClick.bind(this)}/>
-            </label>
+        <div id = 'index'>
+           <AddButton/>
+           <div>
+           </div>
         </div>
         )
     }
---
+++ view/src/js/ui/addButton.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import fileObject from '../utils/storeFiles';
+import {addFile} from '../constants';
+
+class AddButton extends React.Component{
+    handleOnClick(e){
+        e.preventDefault()
+        e.stopPropagation()
+
+        const files = e.currentTarget.files;
+        fileObject.upload(files[0]);
+    }
+    render(){
+        return(
+        <div id = 'buttonDiv'>
+            <label className='addButton'>
+            {addFile}
+                <input accept='true' autoComplete='off' className = 'fileInput' type="file" tabIndex='-1' onChange = {this.handleOnClick.bind(this)}/>
+            </label>
+        </div>
+        )
+    }
+}
+
+export default AddButton
